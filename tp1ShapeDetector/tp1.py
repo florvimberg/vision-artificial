e9de411c627ee33af69446406afcdbad03099cdd
matiasvenditti <mativenditti@gmail.com>
2020-09-18 00:46:43 -0300

    added greatest contour logic and blue display, press P to print hu moments

--- tp1ShapeDetector/tp1.py
+++ tp1ShapeDetector/tp1.py
@@ -17,8 +17,10 @@ import cv2
 
 if __name__ == '__main__': print('TP 1')
 
+
 def on_trackbar_change(val):
     pass
+
 
 window_name = 'Shape detector'
 trackbar_name = 'Trackbar'
@@ -31,7 +33,34 @@ cv2.namedWindow(window_name)
 slider_max2 = 100
 cv2.createTrackbar(trackbar_name2, window_name, 10, slider_max2, on_trackbar_change)
 
-def shapeDetector():
+# Da como resultado el contorno con el area maxima dada una lista de contornos.
+def get_greatest_contour(contours):
+    max_contour = contours[0]
+    for contour in contours:
+        if cv2.contourArea(contour) > cv2.contourArea(max_contour):
+            max_contour = contour
+
+    return max_contour
+
+
+# Calcula los momentos de Hu en base a un contorno
+def get_hu_moments(contour):
+    moments = cv2.moments(contour)
+    return cv2.HuMoments(moments)
+
+
+def print_hu_moments(greatest_contour, saved_contours):
+    if greatest_contour is not None and len(saved_contours) > 0:
+        greatest_hu_moments = get_hu_moments(greatest_contour)
+        saved_shape_hu_moments = get_hu_moments(saved_contours)
+
+        print('Greatest Contour Hu Moments:\n{}'.format(greatest_hu_moments))
+        print('Saved Contours Hu Moments:\n{}'.format(saved_shape_hu_moments))
+    else:
+        print("No se tiene un contorno guardado actualmente, para guardar uno presione F")
+
+
+def shape_detector():
     cap = cv2.VideoCapture(0)
     saved_shape = []
 
@@ -66,20 +95,30 @@ def shapeDetector():
         #   otros momentos, como m00
         #   otros parámetros relevantes, como concavidades
         if len(contours) > 0:
-            moments = cv2.moments(contours[0])
-            huMoments = cv2.HuMoments(moments)
+            greatest_contour = get_greatest_contour(contours)
+
             sensibility_trackbar_val = cv2.getTrackbarPos('Sensibility trackbar', window_name)
 
+            # Se muestra el contorno mas grande en color azul para diferenciar del resto con un grosor mayor.
+            cv2.drawContours(new_frame, [greatest_contour], -1, (255, 0, 0), 12)
+
+            # Con el comando F se guarda el contorno de mayor area.
             if cv2.waitKey(1) & 0xFF == ord('f'):
-                saved_shape = contours[0]
+                saved_shape = greatest_contour
+
+            # Con el comando P se printean los valores de Hu para el contorno de mayor area comparado con el del contorno guardado.
+            if cv2.waitKey(1) & 0xFF == ord('p'):
+                print_hu_moments(greatest_contour, saved_shape)
 
             # 6. Clasificación binaria: Aplicar un criterio sobre los invariantes para decidir si el contorno corresponde o no a la pieza a reconocer
             for contour in contours:
                 # compares hu moments from two contours
                 # 7. Anotar los contornos sobre la imagen monocromática y visualizarla
                 if len(saved_shape) > 0 and cv2.matchShapes(contour, saved_shape, cv2.CONTOURS_MATCH_I2, 0) < (sensibility_trackbar_val/100):
+                    # Todos los contornos cuyos momentos de Hu sean considerados similares a el contorno guardado se muestran en color verde.
                     cv2.drawContours(new_frame, [contour], -1, (0, 255, 0), 3)
                 else:
+                    # Todos los demas contornos se muestran con un contorno en rojo.
                     cv2.drawContours(new_frame, [contour], -1, (0, 0, 255), 3)
 
         cv2.imshow(window_name, cv2.flip(new_frame, 1))
@@ -88,4 +127,4 @@ def shapeDetector():
             break
 
 
-shapeDetector()
+shape_detector()
